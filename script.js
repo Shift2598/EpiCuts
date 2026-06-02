2d58003bd69a9124e2797a2da4bdf4a931461736
Shift2598 <shift2598@users.noreply.github.com>
2026-06-02 18:51:18 +0800

    Change time slots to 1-hour intervals

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ if (monthSelect && daySelect && timeSelect) {
 
     function populateTimes(bookedTimes) {
         timeSelect.innerHTML = '<option value="" disabled selected>Time</option>';
-        const slots = ['09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30'];
+        const slots = ['09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00'];
         const booked = bookedTimes || [];
         slots.forEach(t => {
             const opt = document.createElement('option');
